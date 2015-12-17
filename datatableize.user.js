258a1968cad0b15ee005eae5cf98344968168013
Juan Romero Abelleira <juan@romeroabelleira.com>
2015-12-17 13:04:58 +0100

    remove icon

--- datatableize.user.js
+++ datatableize.user.js
@@ -6,7 +6,6 @@
 // @grant         GM_getResourceText
 // @resource      coffee main.coffee
 // @version       0.1
-// @icon          http://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg/220px-The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg
 // ==/UserScript==
 
 eval(CoffeeScript.compile(GM_getResourceText(resourceName)));
