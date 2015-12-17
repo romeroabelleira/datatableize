87dcbba3a0bbe877050b35bea75fe83626b0f31b
Juan Romero Abelleira <juan@Juans-MacBook-Pro-2.local>
2015-12-17 12:24:33 +0100

    initial commit

---
+++ datatableize.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          Datatableize
+// @namespace     http://juan.one/userscripts
+// @description   Datatableize Helvetas P2P
+// @include       https://p2p.getunik.net/organisations/helvetas/*
+// @grant         GM_getResourceText
+// @resource      coffee main.coffee
+// @version       0.1
+// @icon          http://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg/220px-The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg
+// ==/UserScript==
+
+eval(CoffeeScript.compile(GM_getResourceText(resourceName)));
---
+++ main.coffee
@@ -0,0 +1,11 @@
+datatable_stylesheet = """
+<link href="http://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css" media="screen" rel="stylesheet">
+"""
+datatable_js = """
+<script src="http://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
+"""
+document.head.insertAdjacentHTML('beforeend', datatable_stylesheet + datatable_js)
+# TODO: pick table
+# TODO: run datable on table
+
+$('table').DataTable();
