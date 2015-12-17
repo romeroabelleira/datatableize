044894ad05c408f2032295f53019b6db67e27ff6
Juan Romero Abelleira <juan@romeroabelleira.com>
2015-12-17 13:05:17 +0100

    change resource path

--- datatableize.user.js
+++ datatableize.user.js
@@ -4,8 +4,8 @@
 // @description   Datatableize Helvetas P2P
 // @include       https://p2p.getunik.net/organisations/helvetas/*
 // @grant         GM_getResourceText
-// @resource      coffee main.coffee
 // @version       0.1
+// @resource      coffee https://raw.githubusercontent.com/romeroabelleira/datatableize/master/main.coffee
 // ==/UserScript==
 
 eval(CoffeeScript.compile(GM_getResourceText(resourceName)));
