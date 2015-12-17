79222dccc2f4abff5b38c0ee0d5795d8010730a3
Juan Romero Abelleira <juan@romeroabelleira.com>
2015-12-17 13:05:26 +0100

    bump version

--- datatableize.user.js
+++ datatableize.user.js
@@ -4,8 +4,8 @@
 // @description   Datatableize Helvetas P2P
 // @include       https://p2p.getunik.net/organisations/helvetas/*
 // @grant         GM_getResourceText
-// @version       0.1
 // @resource      coffee https://raw.githubusercontent.com/romeroabelleira/datatableize/master/main.coffee
+// @version       1.0
 // ==/UserScript==
 
 eval(CoffeeScript.compile(GM_getResourceText(resourceName)));
